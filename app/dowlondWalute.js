1e7320186fb399ad96724a8dfd3395ac51929a9f
Bartek <bartlomiej.biel@labnoratory.com>
2016-12-19 14:29:50 +0100

    make new function math for sell walute

--- app/dowlondWalute.js
+++ app/dowlondWalute.js
@@ -1,16 +1,35 @@
-/**
- * Created by student on 15.12.16.
- */
-$(function ()
+var selectedOptio = $('#selectForSell option:selected');
+$('#oldValueForSell').html(getCurrency(selectedOptio.val()));
+$('#selectForSell').change(function ()
 {
+    var selectedOption = $('#selectForSell option:selected');
+    $('#oldValueForSell').html(getCurrency(selectedOption.val()));
+});
+
+function getCurrency(currency)
+{
+    currency = currency.toLowerCase();
+    var returnvalue;
     $.ajax({
-        type: 'GET', url: 'http://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json', success: function (data)
+        type: 'GET', async: false, url: 'https://api.nbp.pl/api/exchangerates/rates/c/' + currency + '/today/?format=json', success: function (data)
         {
-            plnToUsd = data.rates[0].ask;
-            $('#plnToUsd').val(plnToUsd);
-            usdToPln = data.rates[0].bid;
-            $('#usdToPln').val(usdToPln);
+
+            returnvalue = data.rates[0].bid;
         }
+
     });
 
-});
+
+
+
+
+    $('#labelInSell').keyup(function ()
+    {
+        var valueInInput = parseFloat($(this).val());
+        var currentRate = parseFloat(returnvalue);
+        $('#sprzedaz').text((valueInInput * currentRate ).toFixed(2));
+        $('#plnViewInSell').text($(this).val());
+    });
+    return returnvalue;
+
+}
--- app/funkcje.js
+++ app/funkcje.js
@@ -3,25 +3,20 @@ $(function ()
     'use strict';
     var pln = localStorage.getItem('pln');
     $('#pln').html(pln);
+    var usd = localStorage.getItem('usd');
+    $('#usd').html(usd);
 
-
-    $(function ()lStorage	4
+    $('#buttonStartMoney').on('click', function ()
     {
-        $('#buttonStartMoney').on('click', function ()
-        {
-
-            var moneyInPut = document.getElementById('inputStartMoney');
-            localStorage.setItem('pln', moneyInPut.value);
-            $('#pln').html(moneyInPut.value);
+        var moneyInPut = document.getElementById('inputStartMoney');
+        localStorage.setItem('pln', moneyInPut.value);
+        $('#pln').html(moneyInPut.value);
 
 
-        });
     });
-});
 
-
-$(function ()
-{
+    var plnToUsd;
+    var usdToPln;
     $.ajax({
         type: 'GET', url: 'http://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json', success: function (data)
         {
@@ -32,37 +27,54 @@ $(function ()
         }
     });
 
-    $('#pln1').on('keyup', function ()
+
+    $('#moneyInInput').on('keyup', function ()
     {
         var plnn = +$(this).val();
-        $('#total').text((plnn * plnToUsd).toFixed(2));
-        $('#valueEntered').text($(this).val());
+        $('#newWaluteForBuy').text((plnn * plnToUsd).toFixed(2));
+        $('#pln11-count').text($(this).val());
 
+        var math = document.getElementById('moneyInInput');
+       localStorage.setItem('pln11', math.value * plnToUsd);
 
-        var math = document.getElementById('pln1');
-        localStorage.setItem('valueEntered', math.value * plnToUsd);
-        localStorage.setItem('valueEntered', math.value);
-        var valueEntered = localStorage.getItem('valueEntered');
     });
 
 
     $('#save').on('click', function ()
     {
         var startPln = localStorage.getItem('pln');
-        var money = localStorage.getItem('valueEntered');
-        if (Math.round(money * 100) <= Math.round(startPln * 100)) {
+        var MyMoney = localStorage.getItem('pln11');
 
-            localStorage.setItem('pln', startPln - money);
-            var pln = localStorage.getItem('pln');
+        if (Math.round(MyMoney * 100) <= Math.round(startPln * 100)) {
+            parseInt(localStorage.setItem('pln', startPln - MyMoney),10);
+           var pln = parseInt(localStorage.getItem('pln'),10);
             $('#pln').html(pln);
-            var usd = localStorage.getItem('valueEntered' + '#usd');
-            $('#usd').html(usd);
+
+            var walute = document.getElementById('moneyInInput');
+            var sum = save('usd', parseInt(walute.value, 10));
+            console.log(sum);
+            $('#usd').html(sum);
 
 
         } else {
-            alert('Za malo pieniedzy');
+            alert("Za malo pieniedzy");
         }
     });
+    function save(currency, value)
+    {
+        if (localStorage.getItem(currency) == null) {
+            localStorage.setItem(currency, 0);
+
+        }
+        var walletValue = parseInt(localStorage.getItem(currency), 10);
+        var sum = walletValue + value;
+        localStorage.setItem(currency, sum);
+        return sum;
+
+
+    }
+
+
 });
 
 
