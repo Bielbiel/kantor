e59d0da263486e1122dbb13b00daa15574eb39a3
Bartek <bartlomiej.biel@labnoratory.com>
2017-01-23 12:15:19 +0100

    counting function

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -9,25 +9,30 @@
         ctrl.message = 'Trener Cinkciarza';
 
         ctrl.data = {
-            model: null, availableOptions: [{name: 'USD'}, {name: 'EUR'}, {name: 'CHF'}, {name: 'GBP'}
-
+            model: null, availableOptions: [{name: 'USD'}, {name: 'EUR'}, {name: 'CHF'}, {name: 'GBP'}, {name: 'CAD'}
             ]
         };
-
 
         ctrl.getCurrency = function (selectedValue)
         {
             CurrencyService.getCurrency(selectedValue)
                     .then(function (result)
                     {
-                        ctrl.money = result;
-                        ctrl.moneyInSell = result;
+                        ctrl.money = result.rates[0].ask;
+                        ctrl.moneyInSell = result.rates[0].bid;
+
+                        ctrl.buy = parseFloat(ctrl.amountForBay * ctrl.money).toFixed(2);
+                        ctrl.sell = parseFloat(ctrl.amountForSell * ctrl.moneyInSell).toFixed(2);
                     });
+
+
         };
 
         ctrl.wallet = $localStorage.$default([{pln: ctrl.amount}, {eur: 0}, {dol: 0}, {chf: 0}, {gbp: 0}]);
 
         ctrl.amount = 0;
+        ctrl.amountForBay = 0;
+        ctrl.amountForSell = 0;
 
         ctrl.Currencies = function (value)
         {
@@ -36,12 +41,14 @@
                 ctrl[value] = data;
 
             });
-
         };
+
         CurrencyService.allCurrencies().then(function (data)
         {
             ctrl.arrayCurrency = data[0].rates;
         });
+
+
     }
 
     angular.module('cinkciarz')
