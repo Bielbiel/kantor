43a021c666ff76890f825e03f490dd5ab514ec59
Bartek <bartlomiej.biel@labnoratory.com>
2017-01-25 13:49:20 +0100

    finish function math and ngStorage

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -9,8 +9,24 @@
         ctrl.message = 'Trener Cinkciarza';
 
         ctrl.data = {
-            model: null, availableOptions: [{name: 'USD'}, {name: 'EUR'}, {name: 'CHF'}, {name: 'GBP'}, {name: 'CAD'}]
+            model: null, availableOptions: [{name: 'USD'}, {name: 'EUR'}, {name: 'CHF'}, {name: 'GBP'}]
         };
+        ctrl.buttonStart = function ()
+        {
+            ctrl.wallet.pln = ctrl.amount;
+            ctrl.wallet.EUR = 0;
+            ctrl.wallet.USD = 0;
+            ctrl.wallet.CHF = 0;
+            ctrl.wallet.GBP = 0;
+        };
+
+        $localStorage.$default({
+            wallet: {
+                pln: 0, USD: 0, EUR: 0, CHF: 0, GBP: 0
+            }
+        });
+
+        ctrl.wallet = Wallet.getWallet();
 
         ctrl.getCurrency = function (selectedValue)
         {
@@ -20,39 +36,31 @@
                         ctrl.money = result.rates[0].ask;
                         ctrl.moneyInSell = result.rates[0].bid;
 
-                        ctrl.buy = parseFloat(ctrl.amountForBay * ctrl.money).toFixed(2);
+                        ctrl.buyInWindow = parseFloat(ctrl.amountForBay * ctrl.money).toFixed(2);
                         ctrl.sell = parseFloat(ctrl.amountForSell * ctrl.moneyInSell).toFixed(2);
+
+                        ctrl.buy = function ()
+                        {
+                            if (Math.round(ctrl.wallet.pln * 100) >= Math.round(ctrl.buyInWindow * 100)) {
+                                ctrl.wallet[ctrl.data.model] += ctrl.amountForBay;
+                                ctrl.wallet.pln -= ctrl.money * ctrl.amountForBay;
+                            }else {
+                                alert('Za malo pieniedzy :(');
+
+                            }
+                        };
+
+                        ctrl.seller = function ()
+                        {
+                            if (Math.round(ctrl.wallet[ctrl.data.model] * 100) >= Math.round(ctrl.amountForSell * 100)) {
+                                ctrl.wallet[ctrl.data.model] -= ctrl.amountForSell;
+                                ctrl.wallet.pln += ctrl.moneyInSell * ctrl.amountForSell;
+                            } else {
+                                alert('Za malo pieniedzy :(');
+                            }
+                        };
                     });
         };
-
-        $localStorage.$default({
-            wallet: {
-                pln: 0, eur: 0
-            }
-        });
-
-        ctrl.confirmBuy = function ()
-        {
-
-            ctrl.buttonInBuy = ctrl.amountForBay - ctrl.wallet.pln;
-            console.log(ctrl.buttonInBuy);
-
-            ctrl.$storage = $localStorage.$default(ctrl.data.model = ctrl.buy);
-
-        };
-        ctrl.setWalletPln = function ()
-        {
-            ctrl.wallet.pln = ctrl.amount;
-        };
-
-        ctrl.buy = function (code, rate, value)
-        {
-            ctrl.wallet[code] += value;
-            ctrl.wallet.pln -= rate * value;
-        };
-
-
-        ctrl.wallet = Wallet.getWallet();
 
         ctrl.amount = 0;
         ctrl.amountForBay = 0;
@@ -71,7 +79,6 @@
         {
             ctrl.arrayCurrency = data[0].rates;
         });
-
     }
 
     angular.module('cinkciarz')
